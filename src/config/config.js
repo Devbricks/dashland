ea7730cc7bb58f643931fb483968b8ce1a1e9081
Pawel Ochota <pawel.ochota@gmail.com>
2018-11-04 09:05:19 +0100

    Move config to separate file

---
+++ src/config/config.js
@@ -0,0 +1,38 @@
+import Phaser from 'phaser';
+
+// Plugins
+import 'phaser-plugin-game-scale';
+
+import GameScene from '../scenes/game';
+import constants from './constants';
+
+const config = {
+  type: Phaser.AUTO,
+  width: constants.WIDTH,
+  height: constants.HEIGHT,
+  canvas: document.querySelector('#dashland-canvas'),
+  antialias: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 10,
+      },
+      debug: false,
+    },
+  },
+  scene: [GameScene],
+  plugins: {
+    global: [{
+      key: 'GameScalePlugin',
+      plugin: Phaser.Plugins.GameScalePlugin,
+      mapping: 'gameScale',
+      data: {
+        mode: 'fit',
+        resizeCameras: true,
+      },
+    }],
+  },
+};
+
+export default config;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 
-// Plugins
-import GameScalePlugin from 'phaser-plugin-game-scale';
+// Config
+import config from './config/config';
 
 // Styles
 import './styles/global.css';
@@ -9,38 +9,6 @@ import './styles/global.css';
 
 // Utils
 import canvasResizer from './utils/resizer';
-
-import constants from './config/constants';
-import GameScene from './scenes/game';
-
-const config = {
-  type: Phaser.AUTO,
-  width: constants.WIDTH,
-  height: constants.HEIGHT,
-  canvas: document.querySelector('#dashland-canvas'),
-  antialias: true,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: {
-        y: 10,
-      },
-      debug: false,
-    },
-  },
-  scene: [GameScene],
-  plugins: {
-    global: [{
-      key: 'GameScalePlugin',
-      plugin: Phaser.Plugins.GameScalePlugin,
-      mapping: 'gameScale',
-      data: {
-        mode: 'fit',
-        resizeCameras: true,
-      },
-    }],
-  },
-};
 
 const game = new Phaser.Game(config);
 
